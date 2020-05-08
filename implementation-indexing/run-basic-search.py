841517d9efe799ee141566ac2aa585f520039bb2
tk3713 <tkovac45@gmail.com>
2020-05-08 14:02:32 +0200

    Everything working (but not very fast)

--- implementation-indexing/run-basic-search.py
+++ implementation-indexing/run-basic-search.py
@@ -6,24 +6,35 @@ if not os.path.exists("inverted-index.db"):
     handle_indexing()
 
 # TODO: Get real query. TK
-query = "trgovina"
+query = "trgovina pravosodje"
+queries = query.split(" ")
 
+sub_results = {}
 time_before = time.time()
+
 _, data = index_pages()
 manual_results = []
 for row in data:
-    if row[0] != query:
+    if row[0] not in queries:
         continue
     manual_results.append((int(row[2]), row[1], row[3]))
-time_taken = (time.time() - time_before) * 1000
 
-manual_results.sort(key=lambda tup: tup[0])  # sorts in place
-
-results = []
 for row in manual_results:
     frequency, document, indexes = row[0], row[1], row[2]
     snippet = get_snippet(document, indexes.split(","))
-    results.append((frequency, document, snippet))
+
+    if document in sub_results:
+        sub_results[document].append((frequency, document, snippet))
+    else:
+        sub_results[document] = [(frequency, document, snippet)]
+
+time_taken = (time.time() - time_before) * 1000
+
+results = []
+for document, list in sub_results.items():
+    results.append((sum(sub[0] for sub in list), document, "...".join([sub[2] for sub in list])))
+
+results.sort(key=lambda tup: tup[0], reverse=True)  # sorts in place
 
 print(format_results(query, time_taken, results))
 
--- implementation-indexing/run-sqlite-search.py
+++ implementation-indexing/run-sqlite-search.py
@@ -7,24 +7,38 @@ if not os.path.exists("inverted-index.db"):
     handle_indexing()
 
 # TODO: Get real query. TK
-query = "trgovina"
+query = "trgovina pravosodje"
+queries = query.split(" ")
 
 conn = sqlite3.connect('inverted-index.db')
 c = conn.cursor()
-select = "SELECT frequency, documentName, indexes " \
-         "FROM Posting WHERE word = ? " \
-         "ORDER BY frequency DESC " \
-         "LIMIT 10"
-
+sub_results = {}
 time_before = time.time()
-c.execute(select, (query,))
+
+for query_word in queries:
+    select = "SELECT frequency, documentName, indexes " \
+             "FROM Posting WHERE word = ? " \
+             "ORDER BY frequency DESC " \
+             "LIMIT 10"
+
+    c.execute(select, (query_word,))
+
+    for row in c.fetchall():
+        frequency, document, indexes = row[0], row[1], row[2]
+        snippet = get_snippet(document, indexes.split(","))
+
+        if document in sub_results:
+            sub_results[document].append((frequency, document, snippet))
+        else:
+            sub_results[document] = [(frequency, document, snippet)]
+
 time_taken = (time.time() - time_before) * 1000
 
 results = []
-for row in c.fetchall():
-    frequency, document, indexes = row[0], row[1], row[2]
-    snippet = get_snippet(document, indexes.split(","))
-    results.append((frequency, document, snippet))
+for document, list in sub_results.items():
+    results.append((sum(sub[0] for sub in list), document, "...".join([sub[2] for sub in list])))
+
+results.sort(key=lambda tup: tup[0], reverse=True)  # sorts in place
 
 print(format_results(query, time_taken, results))
 
